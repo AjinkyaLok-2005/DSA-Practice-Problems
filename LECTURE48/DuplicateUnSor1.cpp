05c56a40aef84856f4f8b1fc5fb0e90690496710
ajinkya947 <lajinkya645@gmail.com>
2025-01-16 23:12:50 +0530

    Finding the duplicates in the UnSorted Linked List and Removing them Approach-1

---
+++ LECTURE48/DuplicateUnSor1.cpp
@@ -0,0 +1,103 @@
+//REMOVING THE DUPLICATE ELEMENTS FROM THE UNSORTED LINKED LIST
+
+#include<iostream>
+#include<map>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node * next;
+
+    //Constructor
+    Node(int data)
+    {
+        this -> data = data;
+        this -> next = NULL;
+    }
+
+    //deconstructor
+    ~Node()
+    {
+        if(this -> next != NULL)
+        {
+            this -> next = NULL;
+            delete next;
+        }
+    }
+};
+
+void insertAtHead(Node * & head, int data)
+{
+    Node * temp = new Node (data);
+    temp -> next = head;
+    head = temp;
+}
+
+void print(Node * head)
+{
+    Node * temp = head;
+
+    if(head == NULL)
+    {
+        cout << "Empty Linked List " << endl;
+    }
+
+    while(temp != NULL)
+    {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+Node * removeDuplicates(Node * & head)
+{
+    if(head == NULL)
+    {
+        return NULL;
+    }
+
+    map<int, bool> visited;
+
+    Node * curr = head;
+    Node * prev = NULL;
+
+    while(curr != NULL)
+    {
+        if(visited[curr->data] == true)
+        {
+            prev -> next = curr -> next;
+            delete curr;
+            curr = prev -> next;
+        }
+        else
+        {
+            visited[curr->data] = true;
+            prev = curr;
+            curr = curr -> next;
+        }
+    }
+
+    return head;
+}
+
+int main()
+{
+    Node * node1 = new Node(2);
+    Node * head = node1;
+
+    insertAtHead(head, 2);
+    insertAtHead(head, 4);
+    insertAtHead(head, 5);
+    insertAtHead(head, 2); 
+    insertAtHead(head, 4); 
+
+    print(head);
+
+    Node * ans = removeDuplicates(head);
+    print(ans);
+
+    return 0;
+}
