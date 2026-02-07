09ac5f181c53f42d6262cf00a6e03a0601e3426c
ajinkya947 <lajinkya645@gmail.com>
2026-02-07 23:17:19 +0530

    Longest Balanced Substring

---
+++ LeetCode/LongestBalancedSubstr.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool checkBalanced(vector<int>& freq)
+    {
+        int common = 0;
+        for(int i = 0; i < 26; i++)
+        {
+            if(freq[i] == 0)
+                continue;
+            if(common == 0)
+                common = freq[i];
+            else if(freq[i] != common)
+                return false;
+        }
+
+        return true;
+    }
+
+    int longestBalanced(string s) {
+        int n = s.length();
+
+        int maxi = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            vector<int> freq(26, 0);
+
+            for(int j = i; j < n; j++)
+            {
+                freq[s[j] - 'a']++;
+
+                if(checkBalanced(freq))
+                {
+                    maxi = max(maxi, j-i+1);
+                }
+            }
+        }
+        return maxi;
+    }
+};
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    Solution obj;
+    cout << obj.longestBalanced(s);
+
+    return 0;
+}
