0a42c4b7e703a7a0eaeca9d2a296a89415316275
ajinkya947 <lajinkya645@gmail.com>
2026-02-11 23:29:16 +0530

    MyCalander3

---
+++ LeetCode/MyCalander3.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+class MyCalendarThree {
+public:
+    map<int, int>events;
+    MyCalendarThree() {
+        
+    }
+    
+    int book(int startTime, int endTime) {
+        events[startTime] += 1;
+        events[endTime] -= 1;
+
+        int count = 0;
+        int maxInt = 0;
+
+        for(auto &it : events)
+        {
+            count += it.second;
+            maxInt = max(maxInt, count); 
+        }
+
+        return maxInt;
+    }
+};
+
+
+int main() {
+    MyCalendarThree obj;
+    cout << obj.book(10, 20) << endl; // returns 1
+    cout << obj.book(50, 60) << endl; // returns 1
+    cout << obj.book(10, 40) << endl; // returns 2
+    cout << obj.book(5, 15) << endl;  // returns 3
+    cout << obj.book(5, 10) << endl;  // returns 3
+    cout << obj.book(25, 55) << endl; // returns 3
+
+    return 0;
+}
