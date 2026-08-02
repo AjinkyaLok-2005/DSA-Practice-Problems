4321f8477c44315e51ddaeb567d88194a1644c20
ajinkya947 <lajinkya645@gmail.com>
2026-08-02 16:11:35 +0530

    Predict the Winner

--- LeetCode/486.cpp
+++ LeetCode/486.cpp
@@ -5,48 +5,138 @@ using namespace std;
 
 class Solution {
 public:
-    int n;
-    int t[23][23];
+    // APPROACH - 1
 
-    // Returns the maximum score difference (Player1 - Player2)
-    int maxDiff(vector<int>& nums, int l, int r) {
+    int solve1(int i, int j, vector<int>& nums)
+    {
+        //base case
+        if(i > j)
+            return 0;
 
-        if (l == r)
-            return nums[l];
+        if(i == j)
+        {
+            return nums[i];
+        }
 
-        if (t[l][r] != -1)
-            return t[l][r];
+        int take_i = nums[i] + min(solve1(i+2, j, nums), solve1(i+1, j-1, nums));
 
-        int take_left = nums[l] - maxDiff(nums, l + 1, r);
-        int take_right = nums[r] - maxDiff(nums, l, r - 1);
+        int take_j = nums[j] + min(solve1(i+1, j-1, nums), solve1(i, j-2, nums));
 
-        return t[l][r] = max(take_left, take_right);
+        return max(take_i, take_j);
+    }
+
+    // APPROACH - 2
+
+    int solve2(vector<int>& nums, int i, int j)
+    {
+        //base case
+        if(i > j)
+            return 0;
+
+        if(i == j)
+            return nums[i];
+
+        int take_i = nums[i] - solve2(nums, i+1, j);
+
+        int take_j = nums[j] - solve2(nums, i, j-1);
+
+        return max(take_i, take_j);
+    }
+
+    int solve2Mem(vector<int>& nums, int i, int j, vector<vector<int>>& dp)
+    {
+        //base case
+        if(i > j)
+            return 0;
+
+        if(i == j)
+            return nums[i];
+
+        if(dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+
+        int take_i = nums[i] - solve2Mem(nums, i+1, j, dp);
+
+        int take_j = nums[j] - solve2Mem(nums, i, j-1, dp);
+
+        return dp[i][j] = max(take_i, take_j);
+    }
+
+    int solve2Tab(vector<int>& nums)
+    {
+        int n = nums.size();
+
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+        for(int i = n-1; i >= 0; i--)
+        {
+            dp[i][i] = nums[i];
+
+            for(int j = i+1; j < n; j++)
+            {
+                int take_i = nums[i] - dp[i+1][j];
+
+                int take_j = nums[j] - dp[i][j-1];
+
+                dp[i][j] = max(take_i, take_j);
+            }
+        }
+
+        return dp[0][n-1];
+    }
+
+    int solve2TabSO(vector<int>& nums)
+    {
+        int n = nums.size();
+
+        vector<int> dp(n, 0);
+
+        for(int i = n-1; i >= 0; i--)
+        {
+            dp[i] = nums[i];
+
+            for(int j = i+1; j < n; j++)
+            {
+                int take_i = nums[i] - dp[j];
+
+                int take_j = nums[j] - dp[j-1];
+
+                dp[j] = max(take_i, take_j);
+            }
+        }
+
+        return dp[n-1];
     }
 
     bool predictTheWinner(vector<int>& nums) {
-        n = nums.size();
-        memset(t, -1, sizeof(t));
+        int n = nums.size();
 
-        return maxDiff(nums, 0, n - 1) >= 0;
+        // int total_score = accumulate(begin(nums), end(nums), 0);
+        // int p1 = solve(0, n-1, nums);
+        // int p2 = total_score - p1;
+        // return p1 >= p2;
+
+
+        // return solve2(nums, 0, n-1) >= 0;
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        // return solve2Mem(nums, 0, n-1, dp) >= 0;
+
+        // return solve2Tab(nums) >= 0;
+
+        return solve2TabSO(nums) >= 0;
     }
-};
+}; 
 
 int main() {
-    int n;
-
-    cout << "Enter the number of elements: ";
-    cin >> n;
-
-    vector<int> nums(n);
-
-    cout << "Enter the elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
-
     Solution obj;
 
-    if (obj.predictTheWinner(nums))
+    vector<int> nums = {1, 5, 2};
+
+    bool ans = obj.predictTheWinner(nums);
+
+    if (ans)
         cout << "Player 1 can win (or tie)." << endl;
     else
         cout << "Player 1 cannot win." << endl;
