72c5359f8b7133fee0390c83a5b8773ad188ea61
ajinkya947 <lajinkya645@gmail.com>
2026-08-29 21:32:51 +0530

    Palindromic Substrings

---
+++ LeetCode/647.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool solve(string& s, int i, int j)
+    {
+        if(i >= j)
+            return true;
+
+        if(s[i] == s[j])
+            return solve(s, i + 1, j - 1);
+
+        return false;
+    }
+
+    int countSubstrings(string s)
+    {
+        int n = s.length();
+        int count = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i; j < n; j++)
+            {
+                if(solve(s, i, j))
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    string s;
+
+    cout << "Enter string: ";
+    cin >> s;
+
+    Solution obj;
+
+    cout << "Number of palindromic substrings: "
+         << obj.countSubstrings(s) << endl;
+
+    return 0;
+}
